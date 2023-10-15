c2c5749fc8fc9fab19edc0ca7588a5a0d1c49e72
melkadze <25963683+melkadze@users.noreply.github.com>
2023-10-15 03:11:22 -0400

    setup google oauth connection and add connection test

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+// Mongo connection test
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://dbUser:XZ3mxLllH2608Bar@cluster0.yimtka6.mongodb.net/?retryWrites=true&w=majority";
+
+// oAuth connection test
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/userinfo.email'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -8,6 +25,69 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+async function listFiles(authClient) {
+  const oauth2 = google.oauth2({version: 'v2', auth: authClient});
+  oauth2.userinfo.v2.me.get(
+	function(err, res) {
+		console.log(res)
+	}
+  )
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -21,3 +101,5 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+authorize().then(listFiles).catch(console.error);
