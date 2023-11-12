c59054ba02e892c02971ae192761f39b68116ac0
melkadze <25963683+melkadze@users.noreply.github.com>
2023-11-11 23:37:49 -0500

    support saving pref set status

--- routers/auth.js
+++ routers/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
 const passport = require("passport")
+const Preference = require("../models/preference")
 
 //login
 router.get("/login", (req, res) => {
@@ -26,8 +27,20 @@ router.get("/google", passport.authenticate("google", {
 }))
 
 //callback for redirecting
-router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
-	res.redirect("/settings")
+router.get("/google/redirect", passport.authenticate("google"), async (req, res) => {
+	// res.redirect("/settings")
+
+	try {
+		const prefs = await Preference.find({ owner: req.user._id })
+		console.log(prefs)
+		if (prefs.length > 0) {
+			res.redirect("/events")
+		} else {
+			res.redirect("/settings")
+		}
+	} catch (err) {
+		console.log(err)
+	}
 })
 
 module.exports = router
