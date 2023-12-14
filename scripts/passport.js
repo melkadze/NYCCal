7a90080a84f506544438a5e283215e84c2ae72c1
melkadze <25963683+melkadze@users.noreply.github.com>
2023-12-14 13:42:21 -0500

    support hosting

--- scripts/passport.js
+++ scripts/passport.js
@@ -20,8 +20,8 @@ passport.deserializeUser((id, done) => {
 passport.use(
 	// use Google oAuth with our callback
 	new GoogleStrategy({
-		// Google does not support dynamic callback URLs, so we hardcode localhost
-		callbackURL: `http://localhost:${process.env.PORT}/auth/google/redirect`,
+		// Set the URL of our hosted application
+		callbackURL: `http://nyccal.glitch.me/auth/google/redirect`,
 		clientID: process.env.oAuthID,
 		clientSecret: process.env.oAuthSecret
 	}, (accessToken, refreshToken, profile, done) => {
